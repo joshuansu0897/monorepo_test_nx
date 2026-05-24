04355ae96865d3c1f1f8714a2962986796375270
Joshua Saucedo <joshuansu0897@gmail.com>
2026-05-23 19:35:28 -0700

    add: shared libraries

--- packages/my-angular-app/src/app/app.ts
+++ packages/my-angular-app/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { UiShared } from '@monorepo-test/ui-shared';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, UiShared],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
---
+++ packages/utils-common/jest.config.cts
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'utils-common',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/utils-common',
+};
