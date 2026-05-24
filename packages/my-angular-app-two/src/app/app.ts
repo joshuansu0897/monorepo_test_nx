8fed92357f75188812ad45fe3b5f666e26e62c40
Joshua Saucedo <joshuansu0897@gmail.com>
2026-05-23 20:08:50 -0700

    adding: three ui components and commons models and endpoints

--- packages/my-angular-app-two/src/app/app.ts
+++ packages/my-angular-app-two/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
-import { UiShared } from '@monorepo-test/ui-shared';
 
 @Component({
-  imports: [NxWelcome, RouterModule, UiShared],
+  imports: [NxWelcome, RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
